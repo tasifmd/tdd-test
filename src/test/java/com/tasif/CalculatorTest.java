2fdd17dd6704033dde66757f74a4816da8ba77db
tasifmd <tasifmd96@gmail.com>
2021-01-31 18:27:20 +0530

    test with null

--- src/test/java/com/tasif/CalculatorTest.java
+++ src/test/java/com/tasif/CalculatorTest.java
@@ -22,4 +22,20 @@ public class CalculatorTest {
 		int actualSum = calculator.add(actualNumbers);
 		assertEquals(expectedSum, actualSum);
 	}
+
+	@Test(expected = RuntimeException.class)
+	public void addTestWithNull() {
+		Calculator calculator = new Calculator();
+		String expression = "10,20,30," + null;
+		String[] numbers = calculator.splitExpression(expression);
+
+		boolean expectedValidate = true;
+		boolean actualValidate = calculator.validateNumber(numbers);
+		assertEquals(expectedValidate, actualValidate);
+
+		int[] actualNumbers = calculator.numericConvert(numbers);
+		int expectedSum = 0;
+		int actualSum = calculator.add(actualNumbers);
+		assertEquals(expectedSum, actualSum);
+	}
 }
